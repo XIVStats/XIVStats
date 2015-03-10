08fee0e891c39c4d376ea3cb9e895ad386fb8a1b
Jonathan Price <jonathan@jonathanprice.uk>
2015-03-10 18:17:09 +0000

    - Add support for class levels (took a lot of work!)

--- ffxivstats.rb
+++ ffxivstats.rb
@@ -72,24 +72,63 @@ class FFXIVStats
     return gender
   end
 
-  def get_levels
+  # Given a lodestone profile page, return all skill levels of the player
+  # as an array
+  def get_levels(page)
+    levels = Array.new
+    class_id = 0
+    line_no = 1
+    page.each do |line|
+      if line.include? "ic_class_wh24_box"
+        levels[class_id] = page[line_no] # This line contains a class level
+        class_id += 1
+      end
+      line_no += 1
+    end
+
+    # Get rid of junk items
+    levels.delete_at(0)
+    levels.delete_at(0)
+    levels.delete_at(9)
+
+    # Get just the numbers
+    levels.each_with_index { |val, index| levels[index] = val[5..6] }
+
+    # Replace any non-numbers (i.e not trained) to nil
+    levels.each_with_index { |val, index| levels[index] = Integer(val) rescue nil }
+
+    levels
   end
 
   # Given a player object, writes the player's details to the database
   def write_to_db(player)
-    @db.execute("insert into 'players' (id, name, realm, race, gender) values ('#{player.id}','#{player.player_name}','#{player.realm}','#{player.race}','#{player.gender}');")
+    @db.execute("insert into 'players' (id, name, realm, race, gender, level_gladiator, level_pugilist, level_marauder
+      , level_lancer, level_archer, level_rogue, level_conjurer, level_thaumaturge, level_arcanist, level_carpenter
+      , level_blacksmith, level_armorer, level_goldsmith, level_leatherworker, level_weaver, level_alchemist
+      , level_culinarian, level_miner, level_botanist, level_fisher) values ('#{player.id}','#{player.player_name}'
+      ,'#{player.realm}','#{player.race}','#{player.gender}','#{player.level_gladiator}','#{player.level_pugilist}'
+      ,'#{player.level_marauder}','#{player.level_lancer}','#{player.level_archer}','#{player.level_rogue}'
+      ,'#{player.level_conjurer}','#{player.level_thaumaturge}','#{player.level_arcanist}','#{player.level_carpenter}'
+      ,'#{player.level_blacksmith}','#{player.level_armorer}','#{player.level_goldsmith}','#{player.level_leatherworker}'
+      ,'#{player.level_weaver}','#{player.level_alchemist}','#{player.level_culinarian}','#{player.level_miner}'
+      ,'#{player.level_botanist}','#{player.level_fisher}');")
   end
 
   def main
     @db = SQLite3::Database.new( "players.db" )
-    @db.execute("create table if not exists 'players' (id INTEGER,name TEXT,realm TEXT,race TEXT,gender TEXT);")    
+    @db.execute("create table if not exists 'players' (id INTEGER,name TEXT,realm TEXT,race TEXT,gender TEXT
+      ,level_gladiator INTEGER,level_pugilist INTEGER,level_marauder INTEGER,level_lancer INTEGER,level_archer INTEGER
+      ,level_rogue INTEGER,level_conjurer INTEGER,level_thaumaturge INTEGER,level_arcanist INTEGER,level_carpenter INTEGER
+      ,level_blacksmith INTEGER,level_armorer INTEGER,level_goldsmith INTEGER,level_leatherworker INTEGER,level_weaver INTEGER
+      ,level_alchemist INTEGER,level_culinarian INTEGER,level_miner INTEGER,level_botanist INTEGER,level_fisher INTEGER);")    
 
-    for i in 1..100
+    for i in 1..1
       if page = get_player_page(i)
         player_name = get_name(page)
         realm = get_realm(page)
         race = get_race(page)
 	gender = get_gender(page)
+        levels = get_levels(page)
 
         player = Player.new
         player.id = i
@@ -97,6 +136,27 @@ class FFXIVStats
         player.realm = realm
         player.race = race
         player.gender = gender
+        
+        player.level_gladiator = levels[0]
+        player.level_pugilist = levels[1]
+        player.level_marauder = levels[2]
+        player.level_lancer = levels[3]
+        player.level_archer = levels[4]
+        player.level_rogue = levels[5]
+        player.level_conjurer = levels[6]
+        player.level_thaumaturge = levels[7]
+        player.level_arcanist = levels[8]
+        player.level_carpenter = levels[9]
+        player.level_blacksmith = levels[10]
+        player.level_armorer = levels[11]
+        player.level_goldsmith = levels[12]
+        player.level_leatherworker = levels[13]
+        player.level_weaver = levels[14]
+        player.level_alchemist = levels[15]
+        player.level_culinarian = levels[16]
+        player.level_miner = levels[17]
+        player.level_botanist = levels[18]
+        player.level_fisher = levels[19]
 
         puts "ID: #{i} | Name: #{player.player_name} | Realm: #{player.realm} | Race: #{player.race} | Gender: #{player.gender}"
 	write_to_db(player)
