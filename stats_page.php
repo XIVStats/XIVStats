675b53bc9cb2f503184e34609b0b694f7807384f
User Pricetx <pricetx@wright.jonathanprice.org>
2017-03-16 22:32:02 +0000

    Added google analytics header

--- stats_page.php
+++ stats_page.php
@@ -467,6 +467,17 @@ $db->close();
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
       <!-- Compiled and minified JavaScript -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+
+      <!-- Google Analytics -->
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-93918116-1', 'auto');
+        ga('send', 'pageview');
+      </script>
       <style>
           .box-element {
               width: 100%;
