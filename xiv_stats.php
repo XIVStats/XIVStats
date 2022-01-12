83657bd25c05a85102a2f14ba1a459a3917f55d8
Padraig Fahy <p@pf.ie>
2022-01-12 22:21:04 +0000

    Missing <p> tag

--- xiv_stats.php
+++ xiv_stats.php
@@ -1074,7 +1074,7 @@ $db->close();
                     </div>
                     -->
                     <div class="col s12 m4 l4 light region-subtitle">
-                        <a href="https://eu.finalfantasyxiv.com/lodestone/playguide/db/item/50fcfd8c8f8/" class="eorzeadb_link">A REALM REBORN SOUNDTRACK</a>
+                        <p><a href="https://eu.finalfantasyxiv.com/lodestone/playguide/db/item/50fcfd8c8f8/" class="eorzeadb_link">A REALM REBORN SOUNDTRACK</a></p>
                         <span class="region-stat">
                             <?php echo $fmt_soundtrack; ?>
                         </span>
